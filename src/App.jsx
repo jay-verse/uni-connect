4c7d4c624ca7b3da8130efbd547fbedd26734759
jay-verse <jayashree2158@gmail.com>
2026-05-13 23:20:18 +0530

    Fixed imports 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { db } from "./firebase";
+
 import React, { useState,useEffect, useRef, useMemo } from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./firebase";
-import { collection, addDoc } from "firebase/firestore";
-import { useState,  useMemo } from "react";
+
+//import { useState,  useMemo } from "react";
 import {
   Heart, MessageCircle, Bookmark, Share2, Search, Bell, Home, Users,
   Briefcase, Calendar, LogOut, Plus, Edit, Trash2, X, Send, Menu,
